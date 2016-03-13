f65683ad380627be5f3ca169ff37b520d4a92c92
dhog10 <dhog10@googlemail.com>
2016-03-13 14:42:17 +0000

    apples

--- ah1/src/com/civilgamers/ah1/commands/Commands.java
+++ ah1/src/com/civilgamers/ah1/commands/Commands.java
@@ -1,7 +1,44 @@
 package com.civilgamers.ah1.commands;
 
+import com.civilgamers.ah1.base.Util;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
 /**
  * Created by Daniel on 13/03/2016.
  */
-public class Commands {
+public class Commands implements CommandExecutor {
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        Player player = (Player) sender;
+
+        if(cmd.getName().equals("advert")) {
+            playerAdvertCommand(player, args[0]);
+        }else if(cmd.getName().equals("example")){
+            Util.msg(player, "This is a test fam");
+        }else if(cmd.getName().equals("/ah")){
+            gamemodeCommand(player, args[0]);
+        }
+        return true;
+    }
+
+    public boolean gamemodeCommand(Player player, String command){
+        if(command.equals("help")){
+            Util.msg(player, "----//Commands\\\\----");
+            Util.msg(player, "§e/help§7: Lists gamemode commands");
+            Util.msg(player, "--------------------");
+        }else{
+            Util.msg(player, "Command not recognised! Type /help for all available commands.");
+        }
+        return true;
+    }
+
+    public boolean playerAdvertCommand(Player sender, String message){
+        Util.advert(sender, message);
+        return true;
+    }
+
 }
