befc558819051688da3fd933091928d5f099bb16
Callum Armitage <callum.thepro@hotmail.co.uk>
2016-03-13 15:26:04 +0000

    spaced

--- ah1/src/com/civilgamers/ah1/base/Util.java
+++ ah1/src/com/civilgamers/ah1/base/Util.java
@@ -10,24 +10,24 @@ import org.bukkit.entity.Player;
 public class Util {
 
     public static void msg(Player player, String message){
-        player.sendMessage("§e*§4Gamemode§e*§7:" + message);
+        player.sendMessage("§e*§4Gamemode§e*§7: " + message);
     }
 
     public static void msg(CommandSender player, String message){
-        player.sendMessage("§e*§4Gamemode§e*§7:" + message);
+        player.sendMessage("§e*§4Gamemode§e*§7:"  + message);
     }
 
     public static void broadcast(String message){
-        Bukkit.getServer().broadcastMessage("§e*§4Gamemode§e*§7:" + message);
+        Bukkit.getServer().broadcastMessage("§e*§4Gamemode§e*§7: " + message);
     }
 
     public static void advert(String message){
-        Bukkit.getServer().broadcastMessage("§fServer: §eADVERT§7:" + message);
+        Bukkit.getServer().broadcastMessage("§fServer: §eADVERT§7: " + message);
     }
 
     public static void advert(CommandSender sender, String message){
         Player senderPlayer = (Player) sender;
-        Bukkit.getServer().broadcastMessage("§f" + senderPlayer.getDisplayName() + "§eADVERT§7:" + message);
+        Bukkit.getServer().broadcastMessage("§f" + senderPlayer.getDisplayName() + "§eADVERT§7: " + message);
     }
 
 }
