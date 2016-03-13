c54f200eec11cd92982bc7db95ec130e5e5eb80b
dhog10 <dhog10@googlemail.com>
2016-03-13 12:31:23 +0000

    apples

--- src/com/civilgamers/ah1/AH1.java
+++ src/com/civilgamers/ah1/AH1.java
@@ -1,4 +1,15 @@
 package com.civilgamers.ah1;
 
-public class AH1 {
+import java.util.logging.Logger;
+
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.java.JavaPlugin;
+
+public class AH1 extends JavaPlugin{
+
+    public void onEnable(){
+        PluginDescriptionFile pdfFile = getDescription();
+        Logger logger = Logger.getLogger("Minecraft");
+    }
+
 }
