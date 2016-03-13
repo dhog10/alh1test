52047bf615fb68d533fab1d8fdc47a6646f351c3
dhog10 <dhog10@googlemail.com>
2016-03-13 13:29:39 +0000

    apples

---
+++ ah1/src/com/civilgamers/ah1/AH1.java
@@ -0,0 +1,38 @@
+package com.civilgamers.ah1;
+
+import java.util.logging.Logger;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AH1 extends JavaPlugin implements Listener {
+
+    public void onEnable(){
+        PluginDescriptionFile pdfFile = getDescription();
+        Logger logger = Logger.getLogger("Minecraft");
+        getServer().getPluginManager().registerEvents(this, this);
+
+    }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent e) {
+        e.getPlayer().sendMessage("You fucked a block in half");
+        e.getPlayer().setHealth(0);
+    }
+
+    /*public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        Player player = (Player) sender;
+        if(cmd.getName().equals("goto")) {
+            player.teleport(getServer().getPlayer(args[0]));
+        }
+        return true;
+    }*/
+
+}
---
+++ ah1/src/com/civilgamers/ah1/BlockListener.java
@@ -0,0 +1,12 @@
+package com.civilgamers.ah1;
+
+/**
+ * Created by Daniel on 13/03/2016.
+ */
+public class BlockListener {
+    private AH1 plugin;
+
+    public BlockListener(AH1 plugin) {
+        this.plugin = plugin;
+    }
+}
